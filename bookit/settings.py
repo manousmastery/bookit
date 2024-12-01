b6d0c0d5e27ef0bff29006e8d951123c8cdff7f2
Cheikh Omar Faye <40406634+chof63@users.noreply.github.com>
2024-12-01 00:08:52 -0500

    run ruff

--- bookit/settings.py
+++ bookit/settings.py
@@ -27,7 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-CRISPY_TEMPLATE_PACK="bootstrap4"
 
 # Application definition
 
@@ -40,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookit_api',
     'rest_framework',
-    'drf_yasg',
-    "crispy_forms"
 ]
 
 MIDDLEWARE = [
