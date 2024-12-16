d1e30fee3b425953d96faf9d811d7e6bd83c312e
Cheikh Omar Faye <40406634+chof63@users.noreply.github.com>
2024-12-15 23:03:59 -0500

    models

--- bookit_api/models/review.py
+++ bookit_api/models/review.py
@@ -2,13 +2,7 @@ from djongo import models
 
 
 class Review(models.Model):
-    GRADE_CHOICES = [
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5')
-    ]
+    GRADE_CHOICES = [(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]
     review_id = models.AutoField(primary_key=True)
     commentaire = models.TextField()
     grade = models.IntegerField(choices=GRADE_CHOICES, default=0)
--- bookit_api/models/user.py
+++ bookit_api/models/user.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(email, password, **extra_fields)
 
+
 class User(AbstractBaseUser):
     user_id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
